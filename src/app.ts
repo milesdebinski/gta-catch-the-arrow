bdf4b7a61e18b4ad747d3654ff125c5682c7f9d0
Miles Debinski <miles.debinski@gmail.com>
2021-05-18 15:23:04 +0100

    add first arrow test

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+console.log("connected");
+const arrow = document.getElementById("arrow")!;
+const testBtn = document.getElementById("test")!;
+
+testBtn.addEventListener("click", () => {
+  arrow.style.margin = "0px 0px 0px -300px";
+
+  console.log("TEST");
+});
