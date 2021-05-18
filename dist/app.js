20f3a2788d86ac93cfeed18ccabb2622e29fd7dc
Miles Debinski <miles.debinski@gmail.com>
2021-05-18 15:26:25 +0100

    add transition change in ts

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,7 @@ console.log("connected");
 const arrow = document.getElementById("arrow");
 const testBtn = document.getElementById("test");
 testBtn.addEventListener("click", () => {
-    arrow.style.margin = "0px 0px 0px -300px";
-    console.log("TEST");
+  arrow.style.margin = "0px 0px 0px -300px";
+  arrow.style.transition = "all 3s linear 0s";
+  console.log("TEST");
 });
